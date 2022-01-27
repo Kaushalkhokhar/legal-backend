7d466d2c22af9ca6fce5e5b7946004f15372b677
akshitkhokhani9 <akshit107.rejoice@gmail.com>
2022-01-27 18:49:04 +0530

    search query indexing complete

--- server/app.py
+++ server/app.py
@@ -24,113 +24,6 @@ def read_root():
     return {"message": "Welcome to this fantastic app!"}
 
 
-# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-# mydb = myclient["application"]
-# mycol = mydb["users"]
-# mycol.create_index("user_index")
-
-# query_builder = ElasticsearchAPIQueryBuilder()
-
-# es = Elasticsearch(["mongodb://localhost:27017/Users.user_collection"])
-# es = Elasticsearch()
-
-# wait_elasticsearch(es)
-# index_name = "user_index"
-
-
-# @query_builder.filter()
-# def filter_category(c: Optional[str] = Query(None)):
-#     return {
-#         "term": {
-#             "category": c
-#         }
-#     } if c is not None else None
-
-
-# @query_builder.matcher()
-# def match_fields(q: Optional[str] = Query(None)):
-#     return {
-#         "multi_match": {
-#             "query": q,
-#             "fuzziness": "AUTO",
-#             "fields": [
-#                 "name^2",
-#                 "description"
-#             ]
-#         }
-#     } if q is not None else None
-
-
-# @query_builder.sorter()
-# def sort_by(direction: Optional[str] = Query(None)):
-#     return {
-#         "name": direction
-#     } if direction is not None else None
-
-
-# @query_builder.highlighter()
-# def highlight(q: Optional[str] = Query(None),
-#               h: bool = Query(False)):
-#     return {
-#         "name": {}
-#     } if q is not None and h else None
-
-# # import pymongo
-
-# @app.get("/search")
-# async def search(query_body: Dict = Depends(query_builder.build())) -> JSONResponse:
-#     # Search using the Elasticsearch client.
-#     return es.search(
-#         body=query_body,
-#         index=index_name
-#     )
-
-# @app.get("/search")
-# async def search(query_para : Esearch):
-#     query_str = jsonable_encoder(query_para).get("user_query")
-#     print(query_str)
-#     # Search using the Elasticsearch client.
-#     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-#     db = myclient["application"]
-#     # mycol = db["users"]
-#     asd = db.users.find( { "$email": { "$search": query_str } } )
-#     import pdb
-#     pdb.set_trace()
-#     for doc in asd:
-#         print(doc)
-
-#     print(jsonable_encoder(asd))
-#     return jsonable_encoder(asd)
-
-
-# def elastic_user():
-#     users = []
-#     for user in user_collection.find():
-#         users.append(user_helper(user))
-#     return users
-
-# usr = elastic_user()
-# print(usr)
-# usrs_list = retrieve_users()
-# print(usrs_list)
-
-# for usr in usrs_list:
-#     print(usr)
-
-# import pymongo
-
-# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
-# #use database "organisation"
-# mydb = myclient['application']
-# col = mydb['users']
-# print("List of collections\n--------------------")
-# #list the collections
-# for coll in mydb.list_collection_names():
-#     print(coll)
-# import pdb
-# pdb.set_trace()
-
 es = Elasticsearch(HOST="http://localhost", PORT=9200)
 
 mongoclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -144,6 +37,13 @@ for user in cursor:
     es.index(index="user_index", doc_type="text", id=loop, body=user)
     loop +=1
     print(type(user))
+
+# res = es.get(index="user_index", doc_type="text", id=1)
+# # print(res["_source"])
+# # print(res)
+
+# doc_1 = {"sentence":"ak"}
+# es.index(index="english", doc_type="sentences", id=1, body=doc_1)
 
 @app.get("/search")
 async def search(query_para : Esearch):
